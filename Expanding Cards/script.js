580fdf0348f584f3f8fc2e789739cfe87ea92779
Radomir Petkov <radomirpetkov@abv.bg>
2022-10-01 10:38:40 +0300

    finish Expanding cards

---
+++ Expanding Cards/script.js
@@ -0,0 +1,16 @@
+const cards = document.querySelectorAll('.card')
+
+const removeActiveClass = () => {
+    cards.forEach(card => {
+        card.classList.remove('active')
+    })
+}
+
+cards.forEach((card) => {
+    card.addEventListener('click', () => {
+        removeActiveClass()
+        card.classList.add('active')
+    })
+
+})
+
