f3eb846353829727fda5d1f68e84bbdd310a8093
Radomir Petkov <radomirpetkov@abv.bg>
2022-10-02 21:45:31 +0300

    Add Scroll Animation project

---
+++ Scroll Animation/script.js
@@ -0,0 +1,20 @@
+const boxex = document.querySelectorAll('.box')
+
+window.addEventListener(`scroll`, checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight * 0.8
+
+    boxex.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+
+        if (boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}
